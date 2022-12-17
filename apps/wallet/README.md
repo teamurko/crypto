cc82c83647aa94b75083f68b3e27487be59df900
Stanislav Pak <abss.pakso@gmail.com>
2022-12-17 10:45:58 -0800

    Add cli app with balance and latest block info support

---
+++ apps/wallet/README.md
@@ -0,0 +1,10 @@
+### Summary
+This is a demo wallet app for Ethereum network.
+
+### References
+Check https://web3py.readthedocs.io/en/latest/quickstart.html for API docs.
+
+### Usage
+`python3 -m venv .venv`
+`source .venv/bin/activate`
+`deactivate`
---
+++ apps/wallet/cli-app.py
@@ -0,0 +1,46 @@
+import argparse
+from pprint import pprint
+from typing import Optional
+
+from web3 import Web3
+from web3.types import BlockData
+
+
+def main(provider_url: str, action: str, account: Optional[str]) -> None:
+    w3 = _create_client(provider_url)
+    if action not in ACTIONS:
+        raise Exception(f'Unknown action {action}, must be one of {ACTIONS.keys()}')
+    ACTIONS[action](w3, account)
+
+
+def _get_balance(w3: Web3, account: str) -> None:
+    pprint(w3.eth.get_balance(account))
+
+
+def _get_latest_block_data(w3: Web3, *args, **kwargs) -> None:
+    latest_block_info: BlockData = w3.eth.get_block('latest')
+    pprint(latest_block_info)
+
+
+def _create_client(provider_url: str) -> Web3:
+    if provider_url.startswith('https'):
+        return Web3(Web3.HTTPProvider(provider_url))
+    elif provider_url.startswith('wss'):
+        return Web3(Web3.WebsocketProvider(provider_url))
+    else:
+        raise Exception(f'Only https/wss protocols are supported, but provided {provider_url}')
+
+
+ACTIONS = {
+    'get_balance': _get_balance,
+    'get_latest_block_data': _get_latest_block_data,
+}
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Ethereum wallet cli app')
+    parser.add_argument('-p', '--provider', type=str, help='Node provider url, [https|wss]://<url>')
+    parser.add_argument('-a', '--account', type=str, nargs='?', help='Account address')
+    parser.add_argument('-t', '--action_type', type=str, help='Action type: get_latest_block_data, get_balance')
+    args: argparse.Namespace = parser.parse_args()
+    main(provider_url=args.provider, action=args.action_type, account=args.account)
